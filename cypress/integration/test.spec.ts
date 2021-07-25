b2c4dba145b3dd3a50c681f4b7857747d51f8930
Settawat Janpuk <settawat-srs@Settawats-MacBook-Pro.local>
2021-07-25 16:29:07 +0700

    test

---
+++ cypress/integration/test.spec.ts
@@ -0,0 +1,11 @@
+describe('My First Test', () => {
+
+  beforeEach(() => {
+    cy.visit('/')
+  })
+
+  it('Visits the initial project page', () => {
+    // cy.get('#userenameLabel').contains('Username:')
+    cy.get('#h2Resource').contains('Resources:')
+  })
+})
